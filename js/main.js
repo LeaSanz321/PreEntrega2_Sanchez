830a0e9b4528e950e8f170aba15c421ac95b56f6
Leandro Sánchez <leandroukf321@gmail.com>
2023-12-28 23:14:15 -0300

    preEntrega3 terminada

--- js/main.js
+++ js/main.js
@@ -2,17 +2,32 @@ let productosMg = [];
 
 const contenedorMisMg = document.querySelector("#ul_likes");
 
-
 const listaProductos = document.querySelector("#contenedorPadre");
-
 
 
 function agregarMg(evt){
     if(evt.target.classList.contains('like')){
         const producto = evt.target.parentElement;
+        let urlImgProducto = producto.firstElementChild.src.split("/"),
+            imgProducto = urlImgProducto[urlImgProducto.length - 1];
         
-
         mostrarProducto(producto);
+
+        if(localStorage.getItem("lista-productos") != null){
+            let auxLista = window.localStorage.getItem("lista-productos"),
+                fLista = auxLista.concat(imgProducto + "=")
+
+            localStorage.setItem("lista-productos",fLista)
+        }else{
+            localStorage.setItem("lista-productos",imgProducto +"=")
+        }
+
+
+        if(window.localStorage.getItem(urlImgProducto[urlImgProducto.length -1])!=""){
+            window.localStorage.setItem(urlImgProducto[urlImgProducto.length - 1],producto.outerHTML)
+        }else{
+            console.log(window.localStorage.getItem(urlImgProducto[urlImgProducto.length -1]))
+        }
     }
 }
 
@@ -21,11 +36,7 @@ function mostrarProducto(item){
         imagen: item.querySelector("img").src,
         icono: item.querySelector("i").className
     }
-    console.log(infoProducto)
-
     productosMg = [...productosMg, infoProducto];
-        console.log(productosMg)
-
     agregarProducto();
 }
 
@@ -35,12 +46,11 @@ function agregarProducto(){
         newLike.innerHTML = `
             <div class="contenedorImgGal col-sm-6 col-md-4" data-aos="flip-left">
                 <img src="${producto.imagen}" alt="Imagen Campera moda Vintage, Oversize" class="w-100 h-100 object-fit-cover">
-                <i class="like fa-solid fa-heart"></i>
+                <i class="${producto.icono}"></i>
             </div>
         `;
-        contenedorMisMg.appendChild(newLike);
     })
 }
 
 
-listaProductos.addEventListener('click', agregarMg);
+listaProductos.addEventListener('click', agregarMg);
